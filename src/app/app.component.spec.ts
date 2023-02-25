5fbf15cafb063e945c55ffd191d03ee4b529b4f9
Alex Cooper <alex@free.vote>
2023-02-25 11:04:53 +0000

    add @auth0/auth0-angular login, logout buttons inject AuthService to app.component add guide to app.component.html import auth.module into app.module add pre-made server.crt to ssl folder

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'A0-with-SSR-V5'`, () => {
+  it(`should have as title 'a0-with-ssr'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('A0-with-SSR-V5');
+    expect(app.title).toEqual('a0-with-ssr');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('A0-with-SSR-V5 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('a0-with-ssr app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AuthModule } from '@auth0/auth0-angular';
+
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { LoginButtonComponent } from './login-button/login-button.component';
+import { LogoutButtonComponent } from './logout-button/logout-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginButtonComponent,
+    LogoutButtonComponent,
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    AuthModule.forRoot({
+      domain: 'freevote.eu.auth0.com',
+      clientId: 'QJKOZtLJRmxan9fTD7HMQ19IoPUOhUdi',
+      authorizationParams: {
+        redirect_uri: window.location.origin
+      }
+    }),
     AppRoutingModule
   ],
   providers: [],
